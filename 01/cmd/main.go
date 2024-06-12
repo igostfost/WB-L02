70eef9ece5da1fb25e93a5b238fe5980718f625a
iGOSTFOST <alekseygostkin@gmail.com>
2024-06-12 18:36:38 +0300

    01 ex - done

---
+++ 01/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
+func main() {
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		// Вывод ошибки в STDERR
+		fmt.Fprintln(os.Stderr, "Ошибка получения времени с NTP сервера:", err)
+		// Возвращение ненулевого кода выхода в OS
+		os.Exit(1)
+	}
+
+	// Форматирование времени для вывода
+	currentTime := time.Now()
+	fmt.Println("Текущее локальное время:", currentTime.Format("2007-01-02 15:04:05"))
+	fmt.Println("Точное время с NTP сервера:", ntpTime.Format("2007-01-02 15:04:05"))
+}
